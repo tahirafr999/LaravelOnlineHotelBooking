216c0b570e31c4f7b76aad5cf6cca6faeea18d73
tahir afridi <74824067+tahirafr999@users.noreply.github.com>
2022-03-30 22:09:22 +0500

    back all

--- app/Http/Controllers/EcommerceProductController.php
+++ app/Http/Controllers/EcommerceProductController.php
@@ -157,10 +157,10 @@ public function getEcommerceProduct(){
 
 Public function getCountCart(Request $request, $id){
     $AddedProductId = $request->id;
-    // comments back to source code
+
     // if($show = )
  echo "<pre>"; print_r($show); exit;
-            $show = $request->all();
+            // $show = $request->all();
         // Echo "<pre>"; print_r($show); exit;s
   
 }
