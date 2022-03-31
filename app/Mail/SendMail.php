beccbd7b269a50d6761bcef3e3707d596b84667d
tahir afridi <74824067+tahirafr999@users.noreply.github.com>
2022-03-31 21:34:11 +0500

    mail function edit

--- app/Mail/SendMail.php
+++ app/Mail/SendMail.php
@@ -6,6 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+// added
 
 class SendMail extends Mailable
 {
