76ad0ca03ba55c126fbd561121a08a3384a08b63
tahir afridi <74824067+tahirafr999@users.noreply.github.com>
2022-04-01 22:49:03 +0500

    files were added

--- app/Mail/SendMail.php
+++ app/Mail/SendMail.php
@@ -6,6 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+// again added to the files
 
 class SendMail extends Mailable
 {
