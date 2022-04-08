dfeb2b9b8e209c269be7d325e78818ef9d355f68
tahir afridi <74824067+tahirafr999@users.noreply.github.com>
2022-04-08 23:37:41 +0500

    Update wayshop-cart.blade.php

--- resources/views/wayshop-cart.blade.php
+++ resources/views/wayshop-cart.blade.php
@@ -1,13 +1,5 @@
-  <!-- Start Cart  -->
-  <div class="cart-box-main">
-            @if(Session::has('flash_message_error'))
-            <div class="alert alert-sm alert-danger alert-block" role="alert">
-               <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-               </button>
-               <strong>{!! session('flash_message_error') !!}</strong>
-            </div>
-            @endif
+
+
             
             @if(Session::has('flash_message_success'))
             <div class="alert alert-sm alert-success alert-block" role="alert">
